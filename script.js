e716192a5e73759e6db907039c2aa97d7aad8d83
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-09 19:54:23 +0500

    QoL + Fixed canvas sizing issue

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const body = document.body;
 const canvas = document.querySelector(".canvas");
 const selection = document.querySelector(".selection");
 const gridbtn = document.querySelector(".showgrid");
+const clearbtn = document.querySelector(".clear");
+const newcanvasbtn = document.querySelector(".newcanvas");
 
 function createCanvas(size) {
   for (let i = 0; i < size ** 2; i++) {
     const pixel = document.createElement("div");
-    pixelSize = Math.floor(600 / size);
+    pixelSize = 600 / size;
     pixel.style.width = pixelSize + "px";
     pixel.style.height = pixelSize + "px";
     pixel.classList.add("pixel");
@@ -14,17 +16,44 @@ function createCanvas(size) {
   }
 }
 
+function removeCanvas() {
+  let i = 0;
+  while (canvas.hasChildNodes()) {
+    document.querySelector(".pixel").remove();
+    i++;
+  }
+  console.log(i);
+}
+
 function selectCustomColor() {
   return prompt("Enter hex value");
 }
 
-createCanvas(16);
+createCanvas(8);
 let fillColor = "red";
-//TODO:make on mouse down instead of on mouse hover
+let mouseDown = false;
+body.onmousedown = () => (mouseDown = true);
+body.onmouseup = () => (mouseDown = false);
+
 canvas.addEventListener("mouseout", (e) => {
+  if (e.target.classList.contains("pixel") && mouseDown) {
+    e.target.style.backgroundColor = fillColor;
+  }
+});
+
+canvas.addEventListener("click", (e) => {
   if (e.target.classList.contains("pixel")) {
     e.target.style.backgroundColor = fillColor;
-    console.log(e.target);
+  }
+});
+
+newcanvasbtn.addEventListener("click", (e) => {
+  size = prompt("Enter canvas size");
+  if (size <= 32) {
+    removeCanvas();
+    createCanvas(size);
+  } else {
+    alert("Maximum limit is 32");
   }
 });
 
@@ -62,5 +91,11 @@ gridbtn.addEventListener("click", (e) => {
       div.style.borderStyle = "solid";
     }
     toggled = false;
+  }
+});
+
+clearbtn.addEventListener("click", (e) => {
+  for (const div of canvas.children) {
+    div.style.backgroundColor = "white";
   }
 });

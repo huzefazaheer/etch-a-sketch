1844015b7fe2b83fd5cd440ee353f8a441073275
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-09 17:09:36 +0500

    Completed grid

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const body = document.body;
 const canvas = document.querySelector(".canvas");
 
 function createCanvas(size) {
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size ** 2; i++) {
     const pixel = document.createElement("div");
-    pixel.innerHTML = "asd ";
+    pixelSize = Math.floor(600 / size);
+    pixel.style.width = pixelSize + "px";
+    pixel.style.height = pixelSize + "px";
     pixel.classList.add("pixel");
     canvas.appendChild(pixel);
   }
@@ -11,7 +14,10 @@ function createCanvas(size) {
 
 createCanvas(16);
 
-canvas.addEventListener("click", (e) => {
-  e.target.style.backgroundColor = "red";
-  console.log(e.target);
+//TODO:make on mouse down instead of on mouse hover
+canvas.addEventListener("mouseout", (e) => {
+  if (e.target.classList.contains("pixel")) {
+    e.target.style.backgroundColor = "red";
+    console.log(e.target);
+  }
 });
